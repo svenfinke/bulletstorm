ba128cf46039be04c4d90a6fc23805d0470139ba
Sven Finke <sven.finke@mail.com>
2022-07-27 21:40:04 +0200

    fix spawning. add dust

--- Scenes/Demo.gd
+++ Scenes/Demo.gd
@@ -3,18 +3,20 @@ extends Node2D
 var Enemy01 = preload("res://Enemies/Enemy.tscn")
 
 func _on_EnemySpawnTimer_timeout():
-	var minPosX = $Knight/MainCamera.global_position.x - 50
-	var maxPosX = $Knight/MainCamera.global_position.x + get_viewport_rect().size.x + 50
-	var minPosY = $Knight/MainCamera.global_position.y - 50
-	var maxPosY = $Knight/MainCamera.global_position.y + + get_viewport_rect().size.y + 50
 	
-	var posX = rand_range(minPosX, maxPosX)
-	var posY = rand_range(minPosY, maxPosY)
+	var screenWidth = get_viewport_rect().size.x
+	var screenHeight = get_viewport_rect().size.y
 	
-	while(posX in range($Knight/MainCamera.global_position.x, $Knight/MainCamera.global_position.x + get_viewport_rect().size.x) 
-		and posY in range($Knight/MainCamera.global_position.y, $Knight/MainCamera.global_position.y + + get_viewport_rect().size.y)
-	):
-		posX = rand_range(minPosX, maxPosX)
-		posY = rand_range(minPosY, maxPosY)
+	var left = $Knight.global_position.x - (screenWidth / 2)
+	var right = $Knight.global_position.x + (screenWidth / 2)
+	var top = $Knight.global_position.y - (screenHeight / 2)
+	var down = $Knight.global_position.y + (screenHeight / 2)
+	
+	var posX = rand_range(left - 100, right + 100)
+	var posY = rand_range(top - 100, down + 100)
+	
+	while((posX > left and posX < right) and (posY > top and posY < down)):
+		posX = rand_range(left - 100, right + 100)
+		posY = rand_range(top - 100, down + 100)
 
 	Global.instance_node(Enemy01, Vector2(posX, posY), get_parent())
